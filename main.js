32d8e1e11558e3e4eb57c232935f4bfa288a0d8a
Salman Parkar <39862393+iamsalman1403@users.noreply.github.com>
2019-08-22 18:33:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ function openMenu() {
 function closeMenu() {
     document.getElementById('side-menu').style.width = '0px';
 }
+
+$(document).ready(function () {
+    $("#smoothScroll").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1500, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
